bbfc4aca507202fbc85ead7355433cd82b3e7c0d
JmCole19 <jayemcee121@gmail.com>
2021-05-07 10:01:54 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/navbar/navbar';
-import Board from './components/appBoard/topContent';
+import Board from './components/appBoard/content';
 function App() {
   return (
     <div className="App">
